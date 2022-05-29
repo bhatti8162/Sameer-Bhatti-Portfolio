a1ff972aa0f34fa6b5ca961b0ba86062abcbcd15
sam-bodhi-main <samibhatti2000@gmail.com>
2022-05-29 18:06:39 -0400

    seo fix

--- src/Components/Body/Home.js
+++ src/Components/Body/Home.js
@@ -68,7 +68,7 @@ const Home = () => {
       <h1
         style={
           mobileMedia
-            ? { alignSelf: "start", marginTop: "240px" }
+            ? { alignSelf: "start", marginTop: "230px" }
             : { alignSelf: "start" }
         }
       >
